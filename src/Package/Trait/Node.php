3133448bb285457693034815014700c1cdd02c7e
Remco van der Velde <remco@universeorange.com>
2025-12-27 22:18:51 +0100

    p

--- src/Package/Trait/Node.php
+++ src/Package/Trait/Node.php
@@ -66,6 +66,7 @@ trait Node {
                     ddd($collection);
                 }
                 $is_collect = true;
+                $collection = [];
                 continue;
             }
             if($is_collect){
