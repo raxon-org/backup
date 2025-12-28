3afac107af9fe60065629839cbb59e75c69e6795
Remco van der Velde <remco@universeorange.com>
2025-12-28 21:06:22 +0100

    p

--- src/Package/Trait/Node.php
+++ src/Package/Trait/Node.php
@@ -101,7 +101,8 @@ trait Node {
                         }
                         Dir::create($file->dir, Dir::CHMOD);
                         $target = $file->dir . $file->name;
-                        ddd(end($collection));
+                        breakpoint(count($collection));
+                        breakpoint($collection);
                         $collection = implode(PHP_EOL, $collection);
 //                        ddd($collection);
                         $write = gzdecode($collection);
