9e1d809410f66f2982fd6fe05d2370a732fb51a2
Remco van der Velde <remco@universeorange.com>
2026-01-04 15:17:32 +0100

    Data backup and improved node backup

--- src/Package/Trait/Audio.php
+++ src/Package/Trait/Audio.php
@@ -149,6 +149,9 @@ trait Audio {
             $dir->ignore($options->ignore);
         }
         $read = $dir->read($directory, true);
+        if($read === false){
+            throw new Exception('Directory not found: ' . $directory);
+        }
         $read = Sort::list($read)->with(['url' => Sort::ASC]);
         $count = 0;
         $number = 0;
