cb0ab11cf6c85ba9dc3e832eb480831086738d5c
Remco van der Velde <remco@universeorange.com>
2026-01-06 22:27:08 +0100

    Backups of all types except application

--- src/Package/Trait/Setup.php
+++ src/Package/Trait/Setup.php
@@ -4,6 +4,7 @@ namespace Package\Raxon\Backup\Trait;
 use Raxon\App;
 
 use Raxon\Module\Core;
+use Raxon\Module\Dir;
 use Raxon\Module\File;
 
 use Raxon\Node\Module\Node;
@@ -21,6 +22,38 @@ trait Setup {
         /**
          * scan dir if node System.Git has count: 0.
          */
+    }
+
+    public function setup_cron(): void
+    {
+        $object = $this->object();
+        $dir_read = $object->config('project.dir.data') . 'Cron';
+        $dir = new Dir();
+        $list = $dir->read($dir_read);
+        if(is_array($list)){
+            foreach($list as $file){
+                $read = explode("\n", File::read($file->url));
+                $is_found = false;
+                foreach($read as $line){
+                    if(stripos($line, 'raxon/backup all') !== false){
+                        $is_found = true;
+                        break;
+                    }
+                }
+                if($is_found === false){
+                    $read[] = '0 0 0 * 5 root /usr/bin/app raxon/backup all -exclude[]=Audio -exclude[]=Video -exclude[]=Book';
+                    File::write($file->url, implode("\n", $read));
+                }
+            }
+            $command = Core::binary($object) . ' raxon/basic cron restore';
+            Core::execute($object, $command, $output, $notification);
+            if($output){
+                echo $output . PHP_EOL;
+            }
+            if($notification){
+                echo $notification . PHP_EOL;
+            }
+        }
     }
 }
 
--- src/Package/View/Setup/Setup.tpl
+++ src/Package/View/Setup/Setup.tpl
@@ -2,24 +2,4 @@
 {{if(!is.empty($register))}}
 {{Package.Raxon.Backup:Import:role.system()}}
 {{Package.Raxon.Backup:Setup:install()}}
-{{$list = dir.read(config('project.dir.data') + 'Cron')}}
-{{if(is.array($list))}}
-{{foreach($list as $file)}}
-{{$read = explode("\n", file.read($file.url))}}
-{{$is.found = false}}
-{{foreach($read as $line)}}
-{{if(string.position.first.occurrence.case.insensitive($line, 'raxon/backup all'))}}
-{{$is.found = true}}
-{{break()}}
-{{/if}}
-{{/foreach}}
-{{if($is.found === false)}}
-{{$read[] = '0 0 0 * 5 root /usr/bin/app raxon/backup all -exclude[]=Audio -exclude[]=Video -exclude[]=Book'}}
-{{$is.write = file.write($file.url, implode("\n", $read))}}
-{{/if}}
-{{/foreach}}
-{{/if}}
-{{$command = binary() + ' raxon/basic cron restore'}}
-{{$output = execute($command)}}
-{{$output}}
-{{/if}}
+{{Package.Raxon.Backup:Setup:setup.cron()}}
